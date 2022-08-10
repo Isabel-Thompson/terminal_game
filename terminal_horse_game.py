b0f5278217727b78428c49f9021b520f5c62d1b3
Isabel-Thompson <thompson.isabel97@gmail.com>
2022-08-10 08:51:43 -0500

    Add breed function, level up via competition function

--- terminal_horse_game.py
+++ terminal_horse_game.py
@@ -1,5 +1,12 @@
 # Create your own game to practice object oriented programming concepts
 import random
+from statistics import mean
+
+def randomize_sex():
+    if random.randint(0,1) == 0:
+        return True
+    else:
+        return False
 
 class Horse:
     def __init__(self, name, breed, isMale, isAdult):
@@ -41,6 +48,9 @@ class Horse:
         if compet_int == 0:
             self.competitions_won += 1
             print("You won the competition! {name} has won {comp_won} competitions total.".format(name=self.name, comp_won=self.competitions_won))
+            if self.competitions_won % 2 == 0:
+                self.level += 1
+                print("{name} leveled up!".format(name=self.name))
         else:
            print("Sorry, you lost the competition. Better luck next time.")
 
@@ -68,7 +78,7 @@ class Trainer:
     def groomHorse(self):
         print("Which horse do you want to groom?")
         self.listHorses()
-        horse_to_groom = input(" ")
+        horse_to_groom = input("")
         for horse in self.horses:
             if horse.name == horse_to_groom:
                 horse.groomHorse()
@@ -78,12 +88,56 @@ class Trainer:
     def competeHorse(self):
         print("Which horse do you want to compete?")
         self.listHorses()
-        horse_to_compete = input(" ")
+        horse_to_compete = input("")
         for horse in self.horses:
             if horse.name == horse_to_compete:
                 horse.competeHorse()
 
+    def breedHorse(self):
+        print("Which stallion do you want to breed?")
+        stallion_list = ""
+        for horse in self.horses:
+            if horse.male and horse.adult:
+                stallion_list += (horse.name + "    ")
+        print(stallion_list)
+        stallion_to_breed = input("")
+        for horse in self.horses:
+            if horse.name == stallion_to_breed:
+                stallion_to_breed = horse
+
+        print("Which mare do you want to breed?")
+        mare_list = ""
+        for horse in self.horses:
+            if horse.adult and not horse.male:
+                mare_list += (horse.name + "    ")
+        print(mare_list)
+        mare_to_breed = input("")
+        for horse in self.horses:
+            if horse.name == mare_to_breed:
+                mare_to_breed = horse
+        
+        def randomize_breed():
+            if random.randint(0,1) == 0:
+                return "quarter horse"
+            else:
+                return "mustang"
+
+        # foal_level = mean([stallion_to_breed.level, mare_to_breed.level])
+        name_of_foal = input("What do you want to name your foal?\n")
+        new_foal = Horse(name_of_foal, randomize_breed(), randomize_sex(), False)
+        self.addHorse(new_foal)
+        print(new_foal)
+
 # Testing classes
+
+# trainer_name = "Isabel"
+# print("Hi " + trainer_name + ", are you ready to play?")
+# trainer1 = Trainer(trainer_name)
+# horse1_name = "Daisy"
+# horse2_name = "Spirit"
+# horse3_name = "Cloud"
+
+# Playing the game
 trainer_name = input("Welcome to your stable. Enter your name below. \n")
 print("Hi " + trainer_name + ", are you ready to play?")
 trainer1 = Trainer(trainer_name)
@@ -98,15 +152,16 @@ trainer1.addHorse(stallion1)
 trainer1.addHorse(foal1)
 
 while True:
-    action = input("What do you want to do? You can view stable, groom, or compete. Type your answer below.\n")
+    action = input("What do you want to do? You can view stable, groom, breed, or compete. Type your answer below.\n")
     if action == 'groom':
         trainer1.groomHorse()
     if action =='compete':
         trainer1.competeHorse()
     if action == 'view stable':
         trainer1.viewStable()
+    if action == 'breed':
+        trainer1.breedHorse()
     if action == "exit":
         print("Thanks for playing!")
         break
 # To Do
-# Breed method, level up based on number of competitions won
